fe39f592bc5804fb6e62122e46ee7fb4feeef6bc
Đỗ Huy Hòa <dohuyhoa12012001gmail.com>
2026-06-29 17:34:36 +0700

    management category & brand for admin

--- src/api/services/category.ts
+++ src/api/services/category.ts
@@ -8,4 +8,22 @@ export class CategoryService {
   public static readonly getCategoryById: (id: number) => Promise<IGetCategoryResponse> = async (
     id,
   ) => axiosInstance.get<IGetCategoryResponse>(`/categories/${id}`).then((res) => res.data)
+
+  public static readonly createCategory: (
+    name: string,
+    slug: string,
+  ) => Promise<IGetCategoryResponse> = async (name, slug) =>
+    axiosInstance.post<IGetCategoryResponse>('/categories', { name, slug }).then((res) => res.data)
+
+  public static readonly updateCategory: (
+    id: number,
+    name: string,
+    slug: string,
+  ) => Promise<IGetCategoryResponse> = async (id, name, slug) =>
+    axiosInstance
+      .patch<IGetCategoryResponse>(`/categories/${id}`, { name, slug })
+      .then((res) => res.data)
+
+  public static readonly deleteCategory: (id: number) => Promise<void> = async (id) =>
+    axiosInstance.delete<void>(`/categories/${id}`).then((res) => res.data)
 }
